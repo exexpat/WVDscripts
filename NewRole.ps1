855d284acfd7b4ffb9729c0d04ca2598d3ae4d57
fuchsdaniel <fuchs@fuchs3.net>
2020-06-22 18:51:11 -0300

    Missing NewRole.ps1 File
    Attached here the NewRole.ps1, as stated in the video (https://www.youtube.com/watch?v=I8gcl8Zvcps), and works perfectly.

---
+++ NewRole.ps1
@@ -0,0 +1,6 @@
+﻿Connect-AzAccount
+install-module -name az.storage -Force -AllowClobber
+import-module -name az.storage
+install-module -name az.resources -Force
+import-module -name az.resources -Force
+New-AzRoleDefinition -InputFile 'c:\internal_data\azure\azurefilesreadwriterole.json'
